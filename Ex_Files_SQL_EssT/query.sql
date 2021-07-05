ccb0cea4c5e93cc199d52a6a33303fb4b7b0682b
Thomas Nguyen <ntthang123@gmail.com>
2021-07-05 15:19:01 -0700

    update fundamental concepts part

--- Ex_Files_SQL_EssT/query.sql
+++ Ex_Files_SQL_EssT/query.sql
@@ -38,3 +38,51 @@ select * from customer;
 -- 3. Fundamental concepts --
 
 --Creating a table
+
+create table test (a TEXT, b TEXT);
+insert into test values('txt here','txt here');
+
+--Deleting a table
+drop table test;
+drop table if exists test;
+
+--inserting rows
+create table test (a INTEGER, b TEXT, c TEXT);
+insert into test values (1, 'This','Right here');
+
+insert into test (a,b,c) select id, name, description from item;
+
+--deleting rows
+select * from test;
+  --write select query then replace [select] with [delete from]
+
+--NULL values
+select * from test where a is not null;
+
+--constraining columns
+drop table if exists test;
+create table test (a TEXT unique, b TEXT, c TEXT default 'df');
+insert into test (a,b) values ('one', 'two');
+
+--changing the schema
+alter table test add d TEXT;
+select * from test;
+
+--ID columns - different for each system
+
+--filtering data
+select * from Country
+  where population <100000 order by population desc;
+
+--removing duplicates
+select distinct contient from country;
+
+--odering outputs
+select name, continent, region from country order by continent desc, region, name;
+
+--conditional expressions
+select
+  case a when 1 then 'true' else 'false' end as boolA,
+  case b when 1 then 'true' else 'false' end as boolB
+  from booltest
+;
